1fb243ee42c920681d01d7e5efbe8550ee3be829
crorisvanjski4 <crorisvanjski4@crorisvanjski4>
2022-11-16 13:43:13 +0100

    added fullscreen compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import "./App.scss";
 import BarContainer from "./components/Dashboard";
 
+function isFullScreen(): boolean {
+	let params = new URLSearchParams(document.location.search);
+	let isFullScreen = params.get("fullscreen") === "true";
+	return isFullScreen;
+}
+
 function App() {
+	const fullScreen = isFullScreen();
+
 	return (
-		<>
+		<div data-fullscreen={fullScreen ? "" : undefined}>
 			<div className="context">
 				<BarContainer count={15} speed={60} />
 			</div>
@@ -22,7 +30,7 @@ function App() {
 					<li></li>
 				</ul>
 			</div>
-		</>
+		</div>
 	);
 }
 
